d295d0a9f91143b9bd88f085b5e7157d9895e3c7
PrcTcal <hojjang0826@gmail.com>
2021-07-09 18:18:39 +0900

    boj 1722 - 순열의 순서(미해결)

---
+++ math/p1722.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class p1722 {
+    
+    // 미해결 문제
+    // 정답 소스코드 봐도 이해가 안가서 일단 건너뜀
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        int k = 0;
+        boolean[] visit = new boolean[N];
+        int[] arr = new int[N];
+        
+        if(Integer.parseInt(st.nextToken()) == 1){
+            k = Integer.parseInt(st.nextToken());
+        } else {
+            
+        }
+        
+    }    
+}
