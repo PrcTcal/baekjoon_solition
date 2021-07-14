f03a0f5ddbb8f279b4ae8908ec7d4d304920ac27
PrcTcal <hojjang0826@gmail.com>
2021-07-14 14:17:43 +0900

    boj 1094 - 막대기

---
+++ math/p1094.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class p1094 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int sum = 64;
+        Stack<Integer> sticks = new Stack<>();
+        sticks.push(64);
+        while(sum != X){
+            int top = sticks.pop();
+            sum -= top;
+            int tmp = top / 2;
+            if(sticks.isEmpty()){
+                sum = tmp;
+            } else {
+                sum += tmp;
+            }
+            sticks.push(tmp);
+            if(sum < X) {
+                sticks.push(tmp);
+                sum += tmp;
+            }
+        }
+        System.out.println(sticks.size());
+    }
+}
