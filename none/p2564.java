5122b133e397957c701e0f61d7e92338a1e4c5dd
PrcTcal <hojjang0826@gmail.com>
2021-07-16 20:56:57 +0900

    boj 2564 풀이 조건 추가

--- none/p2564.java
+++ none/p2564.java
@@ -24,13 +24,12 @@ public class p2564 {
             sum += r[cdir] == 1 ? dis : lr[cdir] - dis;
             sum -= r[cur[0]] == 1 ? cur[1] : lr[cur[0]] - cur[1];
         } else {
-            if(cur[1] * r[cur[0]] < dis * r[cur[0]]){
+            if(cur[1] * r[cdir] < dis * r[cdir]){
                 sum += Math.abs(cur[1] - dis);
             } else {
                 sum += M * 2 + N * 2 - Math.abs(cur[1] - dis);
             }
         }
-        System.out.println("right : " + sum);
         return sum;
     }
 
@@ -50,24 +49,27 @@ public class p2564 {
             sum += r[cdir] == 1 ? dis : lr[cdir] - dis;
             sum -= r[cur[0]] == 1 ? cur[1] : lr[cur[0]] - cur[1];
         } else {
-            if(cur[1] * r[cur[0]] < dis * r[cur[0]]){
+            if(cur[1] * r[cdir] < dis * r[cdir]){
                 sum += Math.abs(cur[1] - dis);
             } else {
                 sum += M * 2 + N * 2 - Math.abs(cur[1] - dis);
             }
         }
-        System.out.println("left : " + sum);
         return sum;
     }
 
+    // 고려해야할 조건
+    // 1. 사각형을 펴서 직선으로 만든다는 생각으로 시작하면 편하다
+    // 2. 한쪽 방향으로 진행할 경우 반대편 직선에서의 이동 길이는 직선 길이에서 입력 길이를 빼줘야 한다
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         int T = Integer.parseInt(br.readLine());
-        int[][] store = new int[N][2];
+        int[][] store = new int[T][2];
         lr = new int[]{0, M, M, N, N};
+
         for(int i = 0 ; i < T ; i++){
             store[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
