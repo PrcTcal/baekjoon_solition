b4a4b87d4bb9475f0ecb17fb6077117fc6bf0194
PrcTcal <hojjang0826@gmail.com>
2021-09-09 15:39:56 +0900

    comment added

--- backtracking/p2580.java
+++ backtracking/p2580.java
@@ -5,6 +5,7 @@ public class p2580 {
     static int[][] map;
     static StringBuilder sb;
 
+    // 가로, 세로, 블럭에 같은 숫자가 있는지 체크
     public static boolean isPossible(int row, int col, int val){
         for(int i = 0 ; i < 9 ; i++){
             if(map[i][col] == val) return false;
@@ -22,11 +23,14 @@ public class p2580 {
     }
 
     public static void backtracking(int row, int col){
+        // (0,0)에서 시작하여 한 행을 다 체크했으면 다음 행으로 넘어감
         if(col == 9){
             backtracking(row + 1, 0);
             return;
         }
 
+        // 모든 행을 다 체크했으면 출력 후 종료
+        // 프로그램을 종료하지 않으면 다른 경우의 스도쿠도 출력해버림
         if(row == 9){
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
@@ -38,6 +42,7 @@ public class p2580 {
             System.exit(0);
         }
 
+        // 빈칸일때 숫자가 들어갈 수 있는지 체크 후 다음 depth로 넘어감
         if(map[row][col] == 0){
             for(int num = 1 ; num <= 9 ; num++){
                 if(isPossible(row, col, num)){
@@ -45,10 +50,12 @@ public class p2580 {
                     backtracking(row, col + 1);
                 }
             }
+            // 이전 depth로 돌아가기 전에 빈칸이었던 칸을 초기화해야 이전 depth에서 다음 숫자를 넣고 난 후
+            // 다음 depth로 넘어갔을 때 해당 칸이 빈칸인채로 존재할 수 있다.
             map[row][col] = 0;
             return;
         }
-        
+
         backtracking(row, col + 1);
     }
 
